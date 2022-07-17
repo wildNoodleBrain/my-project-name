d85ed2385fa638fcc8cdb5b8a28a122c2ea4f596
wildNoodleBrain <104745283+wildNoodleBrain@users.noreply.github.com>
2022-07-16 21:51:41 -0500

    Up to 29.6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,20 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  //})
+  //export class AppComponent {
+  // 
 })
 export class AppComponent {
-  title = 'my-project-name';
+  name = 'Emily Rockwell';
+  title = 'Angular-While-Sleeping';
+  itemList = ['item1', 'item2', 'item3', 'item4'];
+
+  rectangle = {
+    length: 5,
+    width: 6,
+    area: function () {
+      return this.length * this.width;
+    },
+  }
 }
